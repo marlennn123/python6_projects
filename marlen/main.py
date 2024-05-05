3603572829450677b72918dacb8b82674175f9d4
M-Stark <55349494+M-Stark@users.noreply.github.com>
2024-05-05 09:44:33 +0600

    marlen's project

---
+++ marlen/main.py
@@ -0,0 +1,9 @@
+dfgdsgsdfgdsfg
+sdfg
+#df is your dataframe
+#frac is percentage of data to sample
+#0.2 is 20 percent
+df.sample(frac = 0.2)g
+sd
+fgdsfgsdgsd
+gsdg
---
+++ marlen/models.py
@@ -0,0 +1,5 @@
+gsfhfgd
+hdgfhdfg
+h
+df = pd.DataFrame({"Player" : ["Sach","KingK","Zak"], "Role" : ["Master Batter","Run Machine","Speed Machine"]})ghdfh
+df = pd.DataFrame({"Player" : ["Sach","KingK","Zak"], "Role" : ["Master Batter","Run Machine","Speed Machine"]})hfdhdf
